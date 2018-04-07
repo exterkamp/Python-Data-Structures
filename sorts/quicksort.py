90e0f96402a93d61d73e95b2b894f56f8273fc32
Shane Exterkamp <shaneexterkamp5@gmail.com>
2018-04-07 00:02:43 -0400

    [Feature] Recursive Quicksort algorithm.

---
+++ sorts/quicksort.py
@@ -0,0 +1,48 @@
+class quicksort:
+
+    def recursive_sort(list):
+
+        def quicksort(list, low, high):
+
+            # if the low/high indexes haven't flipped
+            if low < high:
+
+                # partition index, which is sorted
+                partition_index = partition(list, low, high)
+
+                # sort the low side
+                quicksort(list, low, partition_index - 1)
+
+                # sort the high side
+                quicksort(list, partition_index + 1, high)
+
+        def partition(list, low, high):
+
+            # set the pivot
+            pivot = list[high]
+
+            # index of low
+            i = low
+
+            for j in range(low, high):
+
+                # if current is smaller than pivot
+                if list[j] <= pivot:
+
+                    # swap i (less than pivot) and j (greater than pivot)
+                    list[i], list[j] = list[j], list[i]
+
+                    # move up the index
+                    i += 1
+
+            # swap the low index marker (i) and the pivot
+            list[i], list[high] = list[high], list[i]
+
+            return i
+
+        # sort
+        quicksort(list, 0, len(list)-1)
+
+        return list
+
+    recursive_sort = staticmethod(recursive_sort)
---
+++ sorts/sort_unit_tests.py
@@ -0,0 +1,16 @@
+import unittest
+from sorts.quicksort import quicksort
+
+
+class test_quicksort_methods(unittest.TestCase):
+
+    random_list = [7, 9, 3, 24, 84, 12, 3, 4, 20, 39, 8, 2, 4, 99, 24, 4]
+
+    sorted_list = [2, 3, 3, 4, 4, 4, 7, 8, 9, 12, 20, 24, 24, 39, 84, 99]
+
+    def test_quicksort(self):
+        self.assertEqual(quicksort.recursive_sort(self.random_list), self.sorted_list)
+
+
+if __name__ == '__main__':
+    unittest.main()
