180931be646ba149ea3273a9747ef1bb2fe4b0fc
Shane Exterkamp <shaneexterkamp5@gmail.com>
2018-05-02 18:37:21 -0400

    [Feature] Added merge sort and simple test therein.

---
+++ sorts/Mergesort.py
@@ -0,0 +1,34 @@
+class Mergesort:
+
+    def recursive_sort(list):
+
+        def merge_sort(l):
+            
+            if len(l) <= 1:
+                return l
+            
+            left = merge_sort(l[:len(l)//2])
+            right = merge_sort(l[len(l)//2:])
+
+            output = merge(left, right)
+            print('->' + str(output))
+            return output
+        
+        def merge(l1, l2):
+            print(l1,l2)
+            res = []
+
+            while l1 and l2:
+                if l1[0] < l2[0]:
+                    res.append(l1.pop(0))
+                else:
+                    res.append(l2.pop(0))
+            while l1:
+                res.append(l1.pop(0))
+            while l2:
+                res.append(l2.pop(0))
+            return res
+
+        return merge_sort(list)
+
+    recursive_sort = staticmethod(recursive_sort)
--- tests/test_mergesort.py
+++ tests/test_mergesort.py
@@ -1,15 +1,15 @@
 import unittest
-from sorts.Quicksort import Quicksort
+from sorts.Mergesort import Mergesort
 
 
-class test_quicksort_methods(unittest.TestCase):
+class test_mergesort_methods(unittest.TestCase):
 
     random_list = [7, 9, 3, 24, 84, 12, 3, 4, 20, 39, 8, 2, 4, 99, 24, 4]
 
     sorted_list = [2, 3, 3, 4, 4, 4, 7, 8, 9, 12, 20, 24, 24, 39, 84, 99]
 
-    def test_quicksort(self):
-        self.assertEqual(Quicksort.recursive_sort(self.random_list), self.sorted_list)
+    def test_mergesort(self):
+        self.assertEqual(Mergesort.recursive_sort(self.random_list), self.sorted_list)
 
 
 if __name__ == '__main__':
