fe195244c01fa7954812f618a0e8bdb3238b96f3
exterkamps <shaneexterkamp5@gmail.com>
2018-06-11 22:11:39 -0400

    [Feature] Topological sorting for graphs.

--- structures/graph.py
+++ structures/graph.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 class GraphNode():
 
     def __init__(self, val):
@@ -9,3 +11,31 @@ class GraphNode():
     
     def remove_adjacent(self, node):
         self.adjacent_list.remove(node)
+
+class Graph():
+
+    def __init__(self, verticies):
+        self.graph = defaultdict(list)
+        self.verticies = verticies
+    
+    def add_edge(self, source, destination):
+        self.graph[source].append(destination)
+
+    def topological_sort(self):
+        visited = set()
+        stack = []
+
+        def dfs(vertex):
+            visited.add(vertex)
+            for i in self.graph[vertex]:
+                if i not in visited:
+                    dfs(i)
+            
+            stack.insert(0, vertex)
+        
+        for i in range(self.verticies):
+            if i not in visited:
+                dfs(i)
+        
+        return stack
+    
