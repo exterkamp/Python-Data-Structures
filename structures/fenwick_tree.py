a43989a0b5d7b8f6b6b79db961ff71434f8307f6
Shane Exterkamp <shaneexterkamp5@gmail.com>
2018-06-18 22:31:11 -0400

    [Feature] Fenwick Tree for 1D datasets!

---
+++ structures/fenwick_tree.py
@@ -0,0 +1,29 @@
+class FenwickTree:
+
+    def __init__(self, arr):
+        self.tree = [0] * (len(arr)+1)
+        
+        for i in range(len(arr)):
+            self.update(i,arr[i])
+
+    def update(self, index, value):
+        index += 1
+        while index <= len(self.tree)-1:
+            self.tree[index] += value
+            index += index & (-index)
+
+    def sum_of_n(self, index):
+        s = 0
+        index += 1
+
+        while index > 0:
+            s += self.tree[index]
+            index -= index & (-index)
+
+        return s
+    
+    def sum_of_range(self, start, end):
+        # minus 1 from start since the range is (s, e) inclusive on both ends
+        start -= 1
+        print(self.sum_of_n(end), self.sum_of_n(start))
+        return self.sum_of_n(end) - self.sum_of_n(start)
