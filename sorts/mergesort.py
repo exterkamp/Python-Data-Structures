cc30d0365c700cc27d20ec2bc0b3a320d9c4a23d
exterkamps <shaneexterkamp5@gmail.com>
2018-07-02 23:09:48 -0400

    [Refactor] Mergesort queue uses deques.

--- sorts/mergesort.py
+++ sorts/mergesort.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 def mergesort(list):
 
     def sort(l):
@@ -9,22 +11,24 @@ def mergesort(list):
         right = sort(l[len(l)//2:])
 
         output = merge(left, right)
-        # print('->' + str(output))
+        
         return output
     
     def merge(l1, l2):
-        # print(l1,l2)
+
         res = []
+        l1 = deque(l1)
+        l2 = deque(l2)
 
         while l1 and l2:
             if l1[0] < l2[0]:
-                res.append(l1.pop(0))
+                res.append(l1.popleft())
             else:
-                res.append(l2.pop(0))
+                res.append(l2.popleft())
         while l1:
-            res.append(l1.pop(0))
+            res.append(l1.popleft())
         while l2:
-            res.append(l2.pop(0))
+            res.append(l2.popleft())
         return res
 
     return sort(list)
