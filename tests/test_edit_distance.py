ebc1e079b1350bd38c5eb64ecfdbcff75a371d40
exterkamps <shaneexterkamp5@gmail.com>
2018-07-02 22:08:26 -0400

    [Test] Added edit distance test that includes deletion.

--- tests/test_edit_distance.py
+++ tests/test_edit_distance.py
@@ -4,6 +4,7 @@ from algorithms.string.edit_distance import calculate_edit_distance
 class TestEditDistance(unittest.TestCase):
 
     def test_edit_distance(self):
-
         self.assertEqual(['Substitute h', 'Insert l', 'Substitute l'], calculate_edit_distance('hello','teio'))
-
+    
+    def test_deletion_required(self):
+        self.assertEqual(['Delete o', 'Delete n', 'Delete e', 'Delete  '], calculate_edit_distance('two','one two'))
